e16f4f53b056977ed42e2f6b4d1cdaee1d4a0ffc
leonardo-leon123 <62914773+leonardo-leon123@users.noreply.github.com>
2020-11-25 05:08:09 -0600

    Update app.py

--- vi-tech2.0/app.py
+++ vi-tech2.0/app.py
@@ -37,7 +37,7 @@ computervision_client = ComputerVisionClient(endpoint, CognitiveServicesCredenti
 
 #Libreria para vision pip install azure-cognitiveservices-vision-computervision==0.5.0
 
-#config = pdfkit.configuration(wkhtmltopdf="./bin/wkhtmltopdf") 
+config = pdfkit.configuration(wkhtmltopdf="./bin/wkhtmltopdf") 
 #Config para heroku
 
 
@@ -204,9 +204,9 @@ def fiesta():
 @app.route('/<name>/<name2>/<location>') #BODAS TEMPLATE
 def pdf_template(name,name2,location):
     rendered = render_template('pdf_template-bodas.html',name=name,name2=name2,location=location)
-    #pdf = pdfkit.from_string(rendered,False,configuration = config)
+    pdf = pdfkit.from_string(rendered,False,configuration = config)
     #Config para heroku    
-    pdf = pdfkit.from_string(rendered,False)
+    #pdf = pdfkit.from_string(rendered,False)
     response = make_response(pdf)
     response.headers['Content-Type'] = 'application/pdf'
     response.headers['Content-Disposition'] = 'inline; filename=bodas.pdf'    
@@ -216,9 +216,9 @@ def pdf_template(name,name2,location):
 @login_required #XV TEMPLATE
 def pdf_template_xv(name,name2,date,hour):
     rendered = render_template('pdf_template-xv.html',name=name,name2=name2,date=date,hour=hour)
-    #pdf = pdfkit.from_string(rendered,False,configuration = config)
+    pdf = pdfkit.from_string(rendered,False,configuration = config)
     #Config para heroku
-    pdf = pdfkit.from_string(rendered,False)    
+    #pdf = pdfkit.from_string(rendered,False)    
     response = make_response(pdf)
     response.headers['Content-Type'] = 'application/pdf'
     response.headers['Content-Disposition'] = 'inline; filename=xv.pdf'    
@@ -227,9 +227,9 @@ def pdf_template_xv(name,name2,date,hour):
 @login_required #GRAD TEMPLATE
 def pdf_template_grad(name,name2,date,location,school):
     rendered = render_template('pdf_template-grad.html',name=name,name2=name2,date=date,location=location,school=school)
-    #pdf = pdfkit.from_string(rendered,False,configuration = config)
+    pdf = pdfkit.from_string(rendered,False,configuration = config)
     #Config para heroku
-    pdf = pdfkit.from_string(rendered,False)  
+    #pdf = pdfkit.from_string(rendered,False)  
     response = make_response(pdf)
     response.headers['Content-Type'] = 'application/pdf'
     response.headers['Content-Disposition'] = 'inline; filename=graduacion.pdf'    
@@ -239,9 +239,9 @@ def pdf_template_grad(name,name2,date,location,school):
 @login_required #FIESTA OFICINA TEMPLATE
 def pdf_template_fiesta(name,name2,date,location,company,price):
     rendered = render_template('pdf_template-fiesta.html',name=name,name2=name2,date=date,location=location,company=company,price=price)
-    #pdf = pdfkit.from_string(rendered,False,configuration = config)    
+    pdf = pdfkit.from_string(rendered,False,configuration = config)    
     #Config para heroku
-    pdf = pdfkit.from_string(rendered,False)
+    #pdf = pdfkit.from_string(rendered,False)
     response = make_response(pdf)
     response.headers['Content-Type'] = 'application/pdf'
     response.headers['Content-Disposition'] = 'inline; filename=fiesta_godin.pdf'    
